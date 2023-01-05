1f5ca14ff3c46b4f1278844d3b9cf7fa922baa18
Arpit Verma <arpitv970@gmail.com>
2023-01-05 19:25:49 +0530

    connected with MongoDB Atlas

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.use('/api', (req, res, next) => {
-    res.send('Hello World');
-});
-
-app.listen(5000);
+mongoose
+    .connect(
+        'mongodb+srv://admin:EwE7c4KpiUTSmh5c@cluster0.z1nxi1r.mongodb.net/Blog?retryWrites=true&w=majority'
+    )
+    .then(() => {
+        app.listen(5000);
+    })
+    .then(() => {
+        console.log(
+            'Connected to the MongoDB Atlas & listening to localhost:5000'
+        );
+    })
+    .catch((err) => {
+        console.log(err);
+    });
