6080e64bde4553dc45774c41ed29b67222374484
Arpit Verma <arpitv970@gmail.com>
2023-01-08 18:59:06 +0530

    Created frontend directory for the MERN Stack Project

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <h1>Ahh! Shit, here we go again!</h1>
+            <section>
+                <h3>
+                    Before we move any further in the project, make sure of the
+                    following:
+                </h3>
+                <ul>
+                    <li>Create frontend design prototype using Figma</li>
+                    <li>
+                        Learn TailwindCSS & use that only for creating
+                        responsive frontend UI
+                    </li>
+                </ul>
+            </section>
+        </div>
+    );
+}
+
+export default App;
