5f49f5e7271ee5f3322977fc66ffda59ba8bcd82
Arpit Verma <arpitv970@gmail.com>
2023-03-05 13:16:30 +0530

    Added Tailwind CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 
 function App() {
     return (
-        <div className='App'>
-            <h1>Ahh! Shit, here we go again!</h1>
-            <section>
-                <h3>
+        <div className='font-poppins m-auto flex flex-col justify-center items-center'>
+            <h1 className='text-3xl font-bold'>Ahh! Shit, here we go again!</h1>
+            <section className='flex flex-col justify-center items-center text-center'>
+                <h3 className='font-semibold text-xl'>
                     Before we move any further in the project, make sure of the
                     following:
                 </h3>
                 <ul>
-                    <li>Create frontend design prototype using Figma</li>
-                    <li>
+                    <li className='line-through'>
+                        Create frontend design prototype using Figma
+                    </li>
+                    <li className='line-through'>
                         Learn TailwindCSS & use that only for creating
                         responsive frontend UI
                     </li>
